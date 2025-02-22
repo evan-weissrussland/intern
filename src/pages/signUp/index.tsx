94889e2547c59b4b772592f81aeed39bf2e1472e
“evan-weissrussland” <evanwodichka@gmail.com>
2025-02-22 22:01:42 +0300

    fix: refactor scroll for signUp and login pages

--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -1,10 +1,14 @@
-import { PageWrapper, SingUp } from '@/components'
+import { SingUp } from '@/components'
+import { ScrollWrapper } from '@/components/profile-settings'
+import { useWindowWidth } from '@/hooks/useWindowWidth'
 
 export function SignUp() {
+  const windowWidth = useWindowWidth()
+
   return (
-    <PageWrapper>
+    <ScrollWrapper className={''} height={'calc(100vh - 61px)'} mobile={windowWidth <= 360}>
       <SingUp onSubmit={() => {}} />
-    </PageWrapper>
+    </ScrollWrapper>
   )
 }
 
