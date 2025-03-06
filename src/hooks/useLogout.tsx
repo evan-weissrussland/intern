e9fe97eed3e13709957f1eef0032686467131731
“evan-weissrussland” <evanwodichka@gmail.com>
2025-03-06 22:38:17 +0300

    fix: save writed in settingform data  after change avatar (if writted data not transmit to server before change avatar) / delete localstorage after logout

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -12,6 +12,11 @@ export const useLogout = () => {
     logout()
       .unwrap()
       .then(() => {
+        if (typeof window !== 'undefined') {
+          localStorage.removeItem('dataOfBirth')
+          localStorage.removeItem('settingsMyProfile')
+          localStorage.removeItem('settingProfile')
+        }
         void router.push('/login')
       })
   }
